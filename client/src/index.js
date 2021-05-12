1e453b41cdb40d48eab758d6d454dca1cb944b10
Hans <fb_hans@outlook.com>
2021-05-12 20:06:16 +0800

    client-side boilerplate

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style/style.css";
+
+const template = <h1>Hello World react</h1>;
+
+ReactDOM.render(
+  template,
+  document.getElementById("root")
+);
